ef7ccfaa6b730f8cb032797f66f4eac8537bf225
rootoor-dev <rootoor.projects@gmail.com>
2022-06-04 00:17:02 +0000

    Create ScreenSize.java

---
+++ ScreenSize.java
@@ -0,0 +1,19 @@
+// java - get screen size using the Toolkit class
+Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+// the screen height
+screenSize.getHeight();
+
+// the screen width
+screenSize.getWidth();
+
+int screenHeight = screenSize.height;
+int screenWidth = screenSize.width;
+
+
+
+
+
+
+
+
